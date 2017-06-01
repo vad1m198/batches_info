09ba4b5d9dc5e336ae1624e707d134c35dfe239c
vad1m198 <Persidskij_on_ferrari@ukr.net>
2017-06-01 21:32:06 +0300

    pagination

--- src/aura/Batches_Info_List/Batches_Info_ListHelper.js
+++ src/aura/Batches_Info_List/Batches_Info_ListHelper.js
@@ -6,16 +6,19 @@
     	component.set("v.showSpinner", true);
 	    var action = component.get("c.getBatchesReult");
 	    var selectedPeriod = component.find("selectPeriod").get("v.value");
+	    var pageNumber = component.get("v.pageNumber");
 	    if(!selectedPeriod) selectedPeriod = 'All';
-	    action.setParams({"timePeriod" : selectedPeriod,"jobStatus" : jobStatus});
+	    action.setParams({"timePeriod" : selectedPeriod,"jobStatus" : jobStatus,"pageNumber":pageNumber});
 	    
 	    action.setCallback(this, function(response) {
 	        var state = response.getState();
 	        if (component.isValid() && state === "SUCCESS") {
 	            component.set("v.batchInfos", response.getReturnValue());	            
 	        } else {
-	            console.log("loadBatchInfos Failed with state: " + state);
-	            if(state === "ERROR") component.set("v.dataError", response.getError()[0].message);
+	            console.log("loadBatchInfos Failed with state: " + state,response.getError()[0].message);
+	            if(state === "ERROR") {
+	            	this.showMessage(response.getError()[0].message, "error");
+	            }
 	        }
 	        component.set("v.showSpinner", false);
 	    });
@@ -30,10 +33,24 @@
 	            this.loadBatchInfos(component, jobStatus);
 	        } else {
 	            console.log("loadTimePeriodsfunction Failed with state: " + state);
-	            if(state === "ERROR") component.set("v.dataError", response.getError()[0].message);
+	            if(state === "ERROR") {
+	            	this.showMessage(response.getError()[0].message, "error");
+	            }
 	        }
 		});
 		$A.enqueueAction(action);
-    }
+    },
+    refreshPageNumber:function(component){
+    	component.set("v.pageNumber", 0);
+    },
+    showMessage: function(message, severity) {
+		component.set("v.messageText", message);
+		component.set("v.messageSeverity", severity);
+		window.setTimeout(
+		    $A.getCallback(function() {
+		        component.set("v.messageText", "");
+		    }), 3000
+		);
+	},
 
 })
